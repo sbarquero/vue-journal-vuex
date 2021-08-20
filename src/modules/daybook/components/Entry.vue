47309ac6a869a13dbf941120483a9797ee6a2ff6
Santiago Barquero <sbarquero@gmail.com>
2021-08-20 19:02:33 +0200

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+      class="entry-container mb-3 pointer p-2"
+      @click="$router.push({ name: 'entry', params: { id: 10 }})"
+    >
+        <!-- Título -->
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">19</span>
+            <span class="mx-1 fs-5">Agosto</span>
+            <span class="mx-2 fw-light">2021, jueves</span>
+        </div>
+        <!-- Descripción -->
+        <div class="entry-description">
+            Amet irure consectetur eu nulla consectetur aliquip exercitation ex consectetur quis exercitation cupidatat qui ullamco. Do sit ut est officia deserunt occaecat ut dolor deserunt tempor ut. Sint proident deserunt aute anim do nostrud aute incididunt dolor ad Lorem exercitation tempor.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    borde-botton: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),  // 1629478442985
+            date: new Date().toDateString(), // Fri Aug 20 2021
+            text: 'Tempor excepteur labore consequat nostrud eu dolor est. Esse sint aute magna eiusmod veniam do et commodo occaecat. Dolore dolore aliqua anim aliqua elit elit adipisicing esse duis nulla.',
+            picture: null, // https://...
+        },
+        {
+            id: new Date().getTime() + 1000,  // 1629478442985
+            date: new Date().toDateString(), // Fri Aug 20 2021
+            text: 'Mollit aliquip deserunt laborum veniam consequat deserunt deserunt. Id mollit excepteur non laboris aliquip eiusmod ut aliqua amet ex nisi. Adipisicing magna incididunt dolor consequat officia consectetur occaecat aliqua. Irure ullamco ex aliqua enim adipisicing adipisicing sunt ullamco irure commodo id non ut. Id proident irure qui deserunt officia ipsum est id. Id consectetur amet excepteur et officia amet quis tempor sit incididunt.',
+            picture: null, // https://...
+        },
+        {
+            id: new Date().getTime() + 2000,  // 1629478442985
+            date: new Date().toDateString(), // Fri Aug 20 2021
+            text: 'Labore voluptate officia eiusmod anim enim ipsum anim incididunt deserunt velit do dolor laborum tempor. Anim non ut aute occaecat exercitation duis eiusmod eiusmod et eu est amet nulla exercitation. Pariatur esse commodo ex in sit magna. Aliquip eu duis nostrud esse duis ex incididunt dolore minim. Laborum labore officia non sint irure enim elit id. Amet nulla magna voluptate eiusmod amet culpa laboris sit voluptate minim incididunt velit magna sit. Quis enim fugiat nostrud esse.',
+            picture: null, // https://...
+        },
+
+    ]
+})
